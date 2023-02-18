901bccd1fe7985d4cbcfe04b6e4a9b748cc1fe90
Nick Seguin <5571025+nseguin42@users.noreply.github.com>
2023-02-17 21:27:23 -0600

    use packaging.version instead of semver

--- pydo/tasks/PackageUpdater.py
+++ pydo/tasks/PackageUpdater.py
@@ -2,8 +2,7 @@ from pydo.config.PackageUpdaterConfig import PackageUpdaterConfig
 from pydo.tasks.ScriptRunner import ScriptRunner
 from tasks.Task import Task
 from utilities.WithLogging import WithLogging
-import semver;
-
+from packaging import version
 class PackageUpdater(ScriptRunner, Task, WithLogging):
     config: PackageUpdaterConfig
     get_installed_version_script = property(lambda self: self.config.get_installed_version_script)
@@ -17,17 +16,23 @@ class PackageUpdater(ScriptRunner, Task, WithLogging):
         latest_version = self.get_latest_version()
         self.info(f"Installed version: {installed_version}")
         self.info(f"Latest version: {latest_version}")
-        if installed_version.compare(latest_version) < 0:
+        if installed_version < latest_version:
             self.logger().info(f"Updating from version {installed_version} to {latest_version}")
             self.run_script(self.script)
         else:
             self.info("No update available")
 
+    def get_version(self, script):
+        value = self.run_script(script)
+        return version.parse(value)
+
+
     def get_installed_version(self):
         script = self.get_installed_version_script
-        return semver.VersionInfo.parse(self.run_script(script))
+        return self.get_version(script)
+
 
     def get_latest_version(self):
         script = self.get_latest_version_script
-        return semver.VersionInfo.parse(self.run_script(script))
+        return self.get_version(script)
 
